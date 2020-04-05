4f47f46d8cff602189e493a8b446d264ab44f0de
wbc <417780894@qq.com>
2020-04-05 14:54:13 +0800

    youhua

--- test.js
+++ test.js
@@ -4,45 +4,40 @@ const express = require('./like-express')
 const app = express()
 
 app.use((req, res, next) => {
-    console.log('请求开始...', req.method, req.url)
-    next()
+  console.log('请求开始...', req.method, req.url)
+  next()
 })
 
 app.use((req, res, next) => {
-    // 假设在处理 cookie
-    console.log('处理 cookie ...')
-    req.cookie = {
-        userId: 'abc123'
-    }
-    next()
+  // 假设在处理 cookie
+  console.log('处理 cookie ...')
+  req.cookie = { userId: 'abc123' }
+  next()
 })
 
 app.use('/api', (req, res, next) => {
-    console.log('处理 /api 路由')
-    next()
+  console.log('处理 /api 路由')
+  next()
 })
 
 app.get('/api', (req, res, next) => {
-    console.log('get /api 路由')
-    next()
+  console.log('get /api 路由')
+  next()
 })
 
 // 模拟登录验证
 function loginCheck(req, res, next) {
-    setTimeout(() => {
-        console.log('模拟登陆成功')
-        next()
-    })
+  setTimeout(() => {
+    console.log('模拟登陆成功')
+    next()
+  })
 }
 
 app.get('/api/get-cookie', loginCheck, (req, res, next) => {
-    console.log('get /api/get-cookie')
-    res.json({
-        errno: 0,
-        data: req.cookie
-    })
+  console.log('get /api/get-cookie')
+  res.json({ errno: 0, data: req.cookie })
 })
 
 app.listen(8000, () => {
-    console.log('server is running on port 8000')
+  console.log('server is running on port 8000')
 })
